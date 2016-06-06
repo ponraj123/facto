ac119fbb34347e58686463a354ee4029c4efe8a5
ponraj123 <ponraj.bluesky@gmail.com>
2016-06-06 21:56:36 +0530

    Create facto.c

---
+++ facto.c
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class FactorialDemo{
+   public static void main(String args[]){
+     
+      Scanner scanner = new Scanner(System.in);
+      int num = scanner.nextInt();
+      int factorial = fact(num);
+      System.out.println(factorial);
+   }
+   static int fact(int n)
+   {
+       int output;
+       if(n==1)
+       {
+         return 1;
+       }
+       output = fact(n-1)* n;
+       return output;
+   }
+}
